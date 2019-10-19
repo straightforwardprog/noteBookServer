691131a237265378486dacf33f22f19b816d2068
unknown <ibr.bouziane@gmail.com>
2019-10-19 18:07:03 +0200

    first commit

---
+++ src/main/java/com/notebook/notebookServer/NotebookServerApplication.java
@@ -0,0 +1,13 @@
+package com.notebook.notebookServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notebook/notebookServer/NotebookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notebook.notebookServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
